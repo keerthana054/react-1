57e73992aef934aae51c6ad479cb1e1b376007dc
Keerthana <91015385+keerthana054@users.noreply.github.com>
2023-06-30 21:37:14 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Button } from './Button';
+import './Footer.css';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section  className='footer-subscription'>
+        <p className='footer-subscription-heading' >
+            Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className='footer-subscription-text' >
+            You can unsubscribe at any time.
+        </p>
+        <div className='input-areas' >
+            <form>
+                <input 
+                type='email' 
+                name='email' 
+                placeholder='Your Email'  
+                className='footer-input' />
+                <Button buttonStyle='btn--outline'>Subscribe</Button>
+            </form>
+        </div>
+      </section>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper' >
+            <div className='footer-link-items' >
+                <h2>About Us</h2>
+                <Link to='/sign-up'>How it works</Link>
+                <Link to='/'>Testimonials</Link>
+                <Link to='/'>Careers</Link>
+                <Link to='/'>Investors</Link>
+                <Link to='/'>Terms of Services</Link>
+            </div>
+            <div className='footer-link-items' >
+                <h2>Contact Us</h2>
+                <Link to='/'>Contact</Link>
+                <Link to='/'>Supports</Link>
+                <Link to='/'>Destinations</Link>
+                <Link to='/'>Sponsorships</Link>
+            </div>
+        </div>
+        <div className='footer-link-wrapper' >
+            <div className='footer-link-items' >
+                <h2>Videos</h2>
+                <Link to='/'>Submit Videos</Link>
+                <Link to='/'>Ambassadors</Link>
+                <Link to='/'>Agency</Link>
+                <Link to='/'>Influencer</Link>
+            </div>
+            <div className='footer-link-items' >
+                <h2>Social Media</h2>
+                <Link to='/'>Instagram</Link>
+                <Link to='/'>Facebook</Link>
+                <Link to='/'>Youtube</Link>
+                <Link to='/'>Twitter</Link>
+            </div>
+        </div>
+      </div>
+      <section className='social-media' >
+        <div className='social-media-wrap' >
+            <div className='footer-logo' >
+                <Link to='/' className='social-logo' >
+                    TRVL <i className='fab fa-tppo3' ></i>
+                </Link>
+                <small className='website-rights' >TRVL © 2020</small>
+                <div className='social-icons' >
+                    <Link className='social-icon-link facebook'
+                    to='/'
+                    target='_blank'
+                    aria-label='Facebook'
+                    >
+                        <i className='fab fa-facebook-f' ></i>
+                    </Link>
+                    <Link className='social-icon-link instagram'
+                    to='/'
+                    target='_blank'
+                    aria-label='Instagram'
+                    >
+                        <i className='fab fa-instagram' ></i>
+                    </Link>
+                    <Link className='social-icon-link twitter'
+                    to='/'
+                    target='_blank'
+                    aria-label='Twitter'
+                    >
+                        <i className='fab fa-twitter' ></i>
+                    </Link>
+                    <Link className='social-icon-link linkedin'
+                    to='/'
+                    target='_blank'
+                    aria-label='Linkedin'
+                    >
+                        <i className='fab fa-linkedin' ></i>
+                    </Link>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css'
+
+export default function SignUp(){
+    return <h1 className="SignUp" >SIGN UP</h1>;
+}
